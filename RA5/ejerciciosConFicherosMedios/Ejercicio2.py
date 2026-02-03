84b5004f79f70cd3f326ffe4cd7f58bda9c9e75b
ignacio <ignaciosanzmesa@gmail.com>
2026-02-03 10:11:58 +0100

    ra5 v2

--- RA5/ejerciciosConFicherosMedios/Ejercicio2.py
+++ RA5/ejerciciosConFicherosMedios/Ejercicio2.py
@@ -1,8 +1,16 @@
+frecuencias = {}
+
 with open("datos.txt") as file:
     contenido = file.read()
     palabras = contenido.split(" ")
 
     for palabra in palabras:
-        conteo = contenido.count(palabra)
-        print("Frecuencia de", palabra, "es" ,conteo, "vez" if conteo == 1 else "veces")
+        for palabra in palabras:
+            if palabra in frecuencias:
+                frecuencias[palabra] += 1
+            else:
+                frecuencias[palabra] = 1
+
+for palabra, conteo in frecuencias.items():
+    print("Frecuencia de", palabra, "es", conteo, "vez" if conteo == 1 else "veces")
 
